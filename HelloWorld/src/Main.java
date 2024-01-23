a9a88e27df425e87bac8926d6bb8b052fd752f75
Adam Alrasi <adam.alrasi@outlook.com>
2024-01-23 19:35:40 +0000

    Java-Fundamentals

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -7,8 +7,31 @@ public class Main {
          System.out.println(msg[0] +"*"+ msg[1]);
          System.out.println("Hello, World!".substring(7));
 
- */
-        System.out.println(true);
+
+        System.out.println(3 == "3.1".length());
+        System.out.println(Boolean.parseBoolean("true"));
+
+        int a; // declaration
+        a = 2345; // assignment
+        int b = 123; // declaration and initialisation
+        double c = 123.456;
+        String name = "Adam";
+        name += "\sAlrasi";
+        a = 1;
+        System.out.println(name + a);
+
+        String firstName = "Adam";
+        String lastName = "Alrasi";
+        int age = 24;
+
+        System.out.println(firstName + " " + lastName + " " + age);
+
+        // Adam's last name is "Alrasi"
+        System.out.println(firstName + "'s last name is \"" + lastName + "\"");
+*/
+        String msg = "Python is the best programming language";
+        String newMsg = msg.replace("Python", "Java");
+        System.out.println(newMsg);
 
     }
 }
