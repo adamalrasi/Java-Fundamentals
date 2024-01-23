00242f8c9e602f1595bfcc8a3f225985a587f9a6
Adam Alrasi <adam.alrasi@outlook.com>
2024-01-23 20:32:03 +0000

    Java-Fundamentals

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,7 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 /*
          String[] msg = "Hello World!".split(" ");
          System.out.println(msg[0] +"*"+ msg[1]);
@@ -28,10 +31,48 @@ public class Main {
 
         // Adam's last name is "Alrasi"
         System.out.println(firstName + "'s last name is \"" + lastName + "\"");
-*/
+
         String msg = "Python is the best programming language";
         String newMsg = msg.replace("Python", "Java");
         System.out.println(newMsg);
+
+
+        // Managing film certification details
+        int ageOfViewer = 15;
+        if(ageOfViewer < 15){
+            System.out.println("U, PG, and 12 films are available");
+        } else if(ageOfViewer < 18){
+            System.out.println("U, PG, 12 and 15 films are available");
+        } else {
+            System.out.println("all films are available");
+        }
+        // Time of day
+        int timeOfDay = 6;
+        // && means logical AND
+        // || means logical OR
+        if (timeOfDay >= 0 && timeOfDay <= 12) {
+            System.out.println("Good morning!");
+        } else if (timeOfDay >= 12 && timeOfDay <= 18) {
+            System.out.println("Good afternoon!");
+        } else {
+            System.out.println("Good evening!");
+        }
+
+
+        Integer[] myIntArr = {23, 24, 24, 25, 25, 26};
+        java.util.List<Integer> myList = java.util.Arrays.asList(myIntArr);
+        java.util.ArrayList<Integer> myArrList = new java.util.ArrayList<Integer>(myList);
+        myList.set(2, 2);
+        System.out.println(myIntArr[myIntArr.length - 1]);
+        myArrList.add(5);
+        if(myArrList.contains(2)){
+            System.out.println("True");
+        }
+        for(int num: myArrList){
+            System.out.println(num);
+        }
+        */
+
 
     }
 }
